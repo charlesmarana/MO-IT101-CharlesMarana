4c42770f733fe819c2ba6fffb55fc0b623358797
Charles Marana <charles.marana@gmail.com>
2026-03-19 20:39:40 +0800

    setting up java file for employee details display

---
+++ src/Employee.java
@@ -0,0 +1,22 @@
+public class Employee{
+
+    String firstName;
+    String lastName;
+    String birthDate;
+    int employeeID;
+
+
+    public Employee (String firstName, String lastName, String birthDate, int employeeID){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.employeeID = employeeID;
+    
+    }
+public static void main(String[] args) {
+    
+    System.out.println("Hello");
+    System.out.println("How are you?");
+}
+
+}
